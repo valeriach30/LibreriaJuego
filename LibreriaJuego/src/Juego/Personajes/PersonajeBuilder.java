bf2a8246dcc6327e9e19a56ada0519c278dbe298
keisch <71908666+keisch@users.noreply.github.com>
2022-10-12 00:59:52 -0600

    Falta PersonajeBuilder

---
+++ LibreriaJuego/src/Juego/Personajes/PersonajeBuilder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Juego.Personajes;
+
+import Juego.Armas.Arma;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author fabri
+ */
+public class PersonajeBuilder {
+ 
+    private ArrayList <String> apariencias = new ArrayList<>();
+    private ArrayList <Arma> armas = new ArrayList<>();
+    private ArrayList <Arma> hechizos = new ArrayList<>();
+    
+}
